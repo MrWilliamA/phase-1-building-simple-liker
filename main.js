e202db1065a080143680b7bfac54c3b47e6dd989
MrWilliamA <will.archer@hotmail.com>
2022-03-30 15:23:25 +1100

    fin

--- main.js
+++ main.js
@@ -6,6 +6,42 @@ const FULL_HEART = '♥'
 
 
 
+  
+  
+
+mimicServerCall("http://mimicServer.example.comasdasd")
+    .then( (resp) => {
+      const heart = document.querySelectorAll('.like');
+   
+      for (let i = 0; i < heart.length; i++) {
+          heart[i].addEventListener("click", function(e) {      
+            let heartColor = e.target.innerHTML;
+            
+           if(heartColor == EMPTY_HEART) {
+            e.target.innerHTML = FULL_HEART;
+             e.target.classList.add("activated-heart");
+    
+            } else {
+              e.target.innerHTML = EMPTY_HEART;
+              e.target.classList.remove("activated-heart");
+            };
+        
+         mimicServerCall();
+      })
+    }
+  })
+.catch((error) => {
+      console.log(error);
+
+      const errorMsg = setTimeout(function() {
+        document.getElementById('modal').classList.remove("hidden");
+      }, 3000);
+      
+    }
+)
+
+ 
+
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
